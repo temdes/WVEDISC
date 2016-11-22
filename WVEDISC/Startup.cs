0665d38398deca05209a81297af83ca81192e177
tede <tede@WIN-RRC6C3ERMCE>
2016-11-22 18:47:33 +0300

    Add project files.

---
+++ WVEDISC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WVEDISC.Startup))]
+namespace WVEDISC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
